6492daf56cad9405e7c2b21b0b5bb7375d41bd74
Nikita P <nporoykov@gmail.com>
2022-07-13 15:59:56 +0300

    tests added

--- src/test/java/ru/otus/jdbcboot/repositories/BookRepositoryJpaTest.java
+++ src/test/java/ru/otus/jdbcboot/repositories/BookRepositoryJpaTest.java
@@ -1,38 +1,32 @@
 package ru.otus.jdbcboot.repositories;
 
 import lombok.val;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.engine.jdbc.spi.JdbcServices;
-import org.hibernate.engine.jdbc.spi.SqlStatementLogger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.annotation.DirtiesContext;
 import ru.otus.jdbcboot.domain.Book;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 
 @DisplayName("Dao для работы с книгой должно")
 @DataJpaTest
 @Import(BookRepositoryJpa.class)
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class BookRepositoryJpaTest {
 
     private static final long EXPECTED_BOOK_ID = 1L;
     private static final int EXPECTED_BOOKS_COUNT = 2;
+
+    private static final List<String> EXPECTED_BOOKS_TITLES = List.of("War and peace", "Eugene Onegin");
+    private static final List<String> EXPECTED_BOOK_AUTHORS = List.of("Leo Tolstoy", "Alex Pushkin");
+    private static final List<String> EXPECTED_BOOKS_GENRES = List.of("Drama", "Tale");
+    // private static final List<String> EXPECTED_BOOKS_COMMENTS = List.of("One of the most epic drama in the world", "Leo Tolstoy wrote it when he was old boy", "Famous literature composition", "The greatest literature monument in tje world");
 
     @Autowired
     private BookRepositoryJpa repo;
@@ -69,15 +63,13 @@ class BookRepositoryJpaTest {
     @DisplayName("получать все книги")
     @Test
     void getAllBooksTest() {
-        var expectedBook1 = new Book(1, "War and peace", "Leo Tolstoy", "Drama");
-        var expectedBook2 = new Book(2, "Eugene Onegin", "Alex Pushkin", "Drama");
-        List<Book> expectedBookList = new ArrayList<>();
-        expectedBookList.add(expectedBook1);
-        expectedBookList.add(expectedBook2);
         var actualBookList = repo.getAllBooks();
-        assertThat(actualBookList)
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactlyInAnyOrder(expectedBook1, expectedBook2);
+        assertThat(actualBookList).isNotNull().hasSize(EXPECTED_BOOKS_COUNT)
+                .allMatch(b -> EXPECTED_BOOKS_TITLES.stream().anyMatch(title -> b.getTitle().contains(title)))
+                .allMatch(b -> EXPECTED_BOOK_AUTHORS.stream().anyMatch(author -> b.getAuthor().contains(author)))
+                .allMatch(b -> EXPECTED_BOOKS_GENRES.stream().anyMatch(genre -> b.getGenre().contains(genre)))
+                .allMatch(b -> b.getComments() != null);
+        ;
     }
 
     @DisplayName("удалять книгу по ID из БД")
@@ -85,6 +77,6 @@ class BookRepositoryJpaTest {
     void deleteBookByIdTest() {
         assertThatCode(() -> repo.getBookById(1)).doesNotThrowAnyException();
         repo.deleteBookById(1);
-        assertThatCode(() -> repo.getByBookId(1)).isInstanceOf(EmptyResultDataAccessException.class);
+        assertThatCode(() -> repo.getBookById(1)).isInstanceOf(Exception.class);
     }
 }
--- src/test/java/ru/otus/jdbcboot/service/BookOperationsServiceTest.java
+++ src/test/java/ru/otus/jdbcboot/service/BookOperationsServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.jdbcboot.repositories.BookRepositoryJpa;
 import ru.otus.jdbcboot.domain.Book;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,49 +19,46 @@ import static org.mockito.Mockito.when;
 @DisplayName("Тест сервиса BookOperationsService")
 @SpringBootTest
 class BookOperationsServiceTest {
-//
-//    @MockBean
-//    private IOServiceStreams ioService;
-//
-//    @MockBean
-//    private BookRepositoryJpa bookDaoJdbc;
-//
-//    @Autowired
-//    private BookOperationsService bookOperationsService;
-//
-//    @Test
-//    void createBookTest() {
-//        long id = 0;
-//
-//        given(ioService.readString()).willReturn("Anna Karenina;Leo Tolstoy;Drama");
-//
-//        assertThat(bookOperationsService.createBook())
-//                .isEqualTo(id);
-//    }
-//
-//    @Test
-//    void deleteBookByIdTest() {
-//        Book expectedBook = new Book(0, "Anna Karenina", "Leo Tolstoy", "Drama");
-//        bookDaoJdbc.insertBook(expectedBook);
-//        assertThatCode(() -> bookOperationsService.getBookById(0)).doesNotThrowAnyException();
-//        bookOperationsService.deleteBookById(0);
-//        assertThatCode(() -> bookOperationsService.getBookById(0)).isNull();
-//    }
-//
-//    @Test
-//    void getBookByIdTest() {
-//        Book expectedBook = new Book(10, "Anna Karenina", "Leo Tolstoy", "Drama");
-//        when(bookDaoJdbc.getByBookId(10)).thenReturn(Optional.of(expectedBook)); //  дописал А
-//
-//        assertThat(bookOperationsService.getBookById(10))
-//                .isEqualTo(expectedBook);
-//    }
-//
-//    @Test
-//    void printNumberOfAllBooksTest() {
-//    }
-//
-//    @Test
-//    void printAllBooksTest() {
-//    }
+
+    private static final Book EXPECTED_BOOK = new Book(5, "Anna Karenina", "Leo Tolstoy", "Drama");
+
+    @MockBean
+    private IOServiceStreams ioService;
+
+    @MockBean
+    private BookRepositoryJpa bookRepo;
+
+    @Autowired
+    private BookOperationsService bookOperationsService;
+
+    @Test
+    void createBookTest() {
+
+    }
+
+    @Test
+    void deleteBookByIdTest() {
+        Book expectedBook = new Book(0, "Anna Karenina", "Leo Tolstoy", "Drama");
+        bookRepo.insertBook(expectedBook);
+        assertThatCode(() -> bookOperationsService.getBookById(0)).doesNotThrowAnyException();
+        bookOperationsService.deleteBookById(0);
+        assertThatCode(() -> bookOperationsService.getBookById(0)).isNull();
+    }
+
+    @Test
+    void getBookByIdTest() {
+        Book expectedBook = new Book(10, "Anna Karenina", "Leo Tolstoy", "Drama");
+        when(bookRepo.getBookById(10)).thenReturn(expectedBook); //  дописал А
+
+        assertThat(bookOperationsService.getBookById(10))
+                .isEqualTo(expectedBook);
+    }
+
+    @Test
+    void printNumberOfAllBooksTest() {
+    }
+
+    @Test
+    void printAllBooksTest() {
+    }
 }
