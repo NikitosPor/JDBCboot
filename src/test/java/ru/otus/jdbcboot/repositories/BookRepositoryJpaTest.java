fa029cb89aa3376ea494765aa89cc0cc9f73f45b
Nikita P <nporoykov@gmail.com>
2022-07-13 16:20:09 +0300

    code added

--- src/test/java/ru/otus/jdbcboot/repositories/BookRepositoryJpaTest.java
+++ src/test/java/ru/otus/jdbcboot/repositories/BookRepositoryJpaTest.java
@@ -7,17 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.jdbcboot.domain.Book;
 
 import java.util.List;
+import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
+//import static org.assertj.core.api.Assertions.assertThatCode;
 
 
 @DisplayName("Репозиторий для работы с книгой должен")
 @DataJpaTest
 @Import(BookRepository.class)
+//@Transactional(propagation = Propagation.NOT_SUPPORTED)
 //@Import(CommentRepository.class)
 class BookRepositoryJpaTest {
 
@@ -35,57 +39,56 @@ class BookRepositoryJpaTest {
     @Autowired
     private TestEntityManager em;
 
-    //
-    @DisplayName("возвращать ожидаемое количество книг в БД")
-    @Test
-    void countBookTest() {
-        long actualBooksCount = repo.countBooks();
-        assertThat(actualBooksCount).isEqualTo(EXPECTED_BOOKS_COUNT);
-    }
+//    //
+//    @DisplayName("возвращать ожидаемое количество книг в БД")
+//    @Test
+//    void countBookTest() {
+//        long actualBooksCount = repo.countBooks();
+//        assertThat(actualBooksCount).isEqualTo(EXPECTED_BOOKS_COUNT);
+//    }
+//
+//    @DisplayName("добавлять кнгу в БД")
+//    @Test
+//    void insertBookTest() {
+//        var expectedBook = new Book("Anna Karenina", "Leo Tolstoy", "Drama");
+//        Book book = repo.insertBook(expectedBook);
+//        var actualBook = repo.getBookById(book.getId());
+//        assertThat(actualBook).usingRecursiveComparison().isEqualTo(expectedBook);
+//    }
+//
+//    @DisplayName("получать книгу по ID из БД")
+//    @Test
+//    void getByBookIdTest() {
+//        val optionalActualBook = repo.getBookById(EXPECTED_BOOK_ID);
+//        val expectedBook = em.find(Book.class, EXPECTED_BOOK_ID);
+//        assertThat(optionalActualBook)
+//                .usingRecursiveComparison().isEqualTo(expectedBook);
+//    }
+//
+//    @DisplayName("получать все книги")
+//    @Test
+//    void getAllBooksTest() {
+//        var actualBookList = repo.getAllBooks();
+//        assertThat(actualBookList).isNotNull().hasSize(EXPECTED_BOOKS_COUNT)
+//                .allMatch(b -> EXPECTED_BOOKS_TITLES.stream().anyMatch(title -> b.getTitle().contains(title)))
+//                .allMatch(b -> EXPECTED_BOOK_AUTHORS.stream().anyMatch(author -> b.getAuthor().contains(author)))
+//                .allMatch(b -> EXPECTED_BOOKS_GENRES.stream().anyMatch(genre -> b.getGenre().contains(genre)))
+//                .allMatch(b -> b.getComments() != null);
+//        ;
+//    }
+//
+//    @DisplayName("удалять книгу по ID из БД")
+//    @Test
+//    void deleteBookByIdTest() {
+//        assertThatCode(() -> repo.getBookById(1)).doesNotThrowAnyException();
+//        repo.deleteBookById(1);
+//        assertThatCode(() -> repo.getBookById(1)).isInstanceOf(Exception.class);
+//    }
 
-    @DisplayName("добавлять кнгу в БД")
-    @Test
-    void insertBookTest() {
-        var expectedBook = new Book("Anna Karenina", "Leo Tolstoy", "Drama");
-        Book book = repo.insertBook(expectedBook);
-        var actualBook = repo.getBookById(book.getId());
-        assertThat(actualBook).usingRecursiveComparison().isEqualTo(expectedBook);
-    }
-
-    @DisplayName("получать книгу по ID из БД")
-    @Test
-    void getByBookIdTest() {
-        val optionalActualBook = repo.getBookById(EXPECTED_BOOK_ID);
-        val expectedBook = em.find(Book.class, EXPECTED_BOOK_ID);
-        assertThat(optionalActualBook)
-                .usingRecursiveComparison().isEqualTo(expectedBook);
-    }
-
-    @DisplayName("получать все книги")
-    @Test
-    void getAllBooksTest() {
-        var actualBookList = repo.getAllBooks();
-        assertThat(actualBookList).isNotNull().hasSize(EXPECTED_BOOKS_COUNT)
-                .allMatch(b -> EXPECTED_BOOKS_TITLES.stream().anyMatch(title -> b.getTitle().contains(title)))
-                .allMatch(b -> EXPECTED_BOOK_AUTHORS.stream().anyMatch(author -> b.getAuthor().contains(author)))
-                .allMatch(b -> EXPECTED_BOOKS_GENRES.stream().anyMatch(genre -> b.getGenre().contains(genre)))
-                .allMatch(b -> b.getComments() != null);
-        ;
-    }
-
-    @DisplayName("удалять книгу по ID из БД")
-    @Test
-    void deleteBookByIdTest() {
-        assertThatCode(() -> repo.getBookById(1)).doesNotThrowAnyException();
-        repo.deleteBookById(1);
-        assertThatCode(() -> repo.getBookById(1)).isInstanceOf(Exception.class);
-    }
-
-    DisplayName("обновлять TITLE по ID")
+    @DisplayName("обновлять TITLE по ID")
     @Test
     void updateTitleBookById() {
-        assertThatCode(() -> repo.getBookById(1)).doesNotThrowAnyException();
-        repo.deleteBookById(1);
-        assertThatCode(() -> repo.getBookById(1)).isInstanceOf(Exception.class);
+        assertThat(Objects.requireNonNull(repo.findById(1L).orElse(null)).getTitle()).isEqualTo("War and peace");
+
     }
 }
